fd59b01b609b9ec46a7eeb758ef2a4596e59c667
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-04-25 02:15:13 +0300

    added aninstruction code in run.py

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 from user import User
 import random
 
+def main():
+     while true:
+         print("Welcome!")
+         print('/n')
+         print("To add a new user,kindlyselect'newuser' :To login into your account,kindly select 'login'.And to leave your account,kindly select 'x' to exit.Thankyou.")
+
