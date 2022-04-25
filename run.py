00601b7f3bef85efadbb912046296b0be4abdd97
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-04-25 03:40:44 +0300

    added if/else

--- run.py
+++ run.py
@@ -5,5 +5,35 @@ def main():
      while true:
          print("Welcome!")
          print('/n')
-         print("To add a new user,kindlyselect'newuser' :To login into your account,kindly select 'login'.And to leave your account,kindly select 'x' to exit.Thankyou.")
+         print("Use short code To add a new user,kindlyselect'newuser' :To login into your account,kindly select 'login'.And to leave your account,kindly select 'x' to exit.Thankyou.")
+
+         short_code = input().lower()
+         print['z']
+
+         if short_code == 'newuser':
+             print('create username')
+             created_user_name = input()
+
+             print('create password')
+             created_user_password= input()
+
+             print('confirm password')
+             confirm_password=input()
+
+             while confirm_password != created_user_password:
+             print("sorry!Password did not match")
+             print("please re-enter password")
+             created_user_password = input()
+             print("confirm password")
+             confirm_password= input()
+             
+             else:
+                 print(f"congratulations {created_user_name},account successfully created!")
+                 print('\n')
+                 print("login")
+                 print("Username")
+                 entered_username = input()
+                 
+
+
 
--- user.py
+++ user.py
@@ -5,8 +5,8 @@ class User:
     '''
 
     user_list = []
-    def __init__(self,userName,password):
-        self.userName= userName
+    def __init__(self,user_name,password):
+        self.user_name= user_name
         self.password= password
 
         def save_user(self):
